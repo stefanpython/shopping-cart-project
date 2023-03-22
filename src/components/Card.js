7b0a49bd744fe92275dbbf31edf43e80f5ee84a1
stefanpython <stefanhtml@yahoo.com>
2023-03-22 16:31:30 +0200

    Display data in card, add resize responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card--container">
       <div className="card--content">
-        <img
-          className="card--img"
-          src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-          alt="t-shirt"
-        />
+        <img className="card--img" src={props.imageUrl} alt="t-shirt" />
         <hr />
-        <p className="card--title">Mens Casual Premium Slim Fit T-Shirts</p>
-        <p className="card--price">$22.3</p>
+        <p className="card--title">{props.title}</p>
+        <p className="card--price">${props.price}</p>
 
         <button className="card--button">Add to cart</button>
       </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -6,18 +6,27 @@ const Shop = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => setData(json));
+    Promise.all([
+      fetch("https://fakestoreapi.com/products/category/women's clothing"),
+      fetch("https://fakestoreapi.com/products/category/men's clothing"),
+    ])
+      .then((responses) => Promise.all(responses.map((res) => res.json())))
+      .then((data) => setData(data.flat()))
+      .catch((error) => console.error(error));
   }, []);
 
   console.log(data);
 
-  return (
-    <div className="shop--container">
-      <Card />
-    </div>
-  );
+  const cardItems = data.map((item) => (
+    <Card
+      imageUrl={item.image}
+      title={item.title}
+      price={item.price}
+      key={item.id}
+    />
+  ));
+
+  return <div className="shop--container">{cardItems}</div>;
 };
 
 export default Shop;
